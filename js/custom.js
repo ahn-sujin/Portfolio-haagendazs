be3432913b0e58e1c123c4c80e90c25578d44125
ahn-sujin <67556491+ahn-sujin@users.noreply.github.com>
2020-11-30 17:57:48 +0900

    upload file
    upload file

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+$(function(){
+    
+    var mySwiper = new Swiper('#flavor .left_box', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+    
+    var mySwiper = new Swiper('#flavor .right_box', {
+        loop: true,
+        effect : 'fade',
+        navigation: {
+            nextEl: '#flavor .swiper-button-next',
+            prevEl: '#flavor .swiper-button-prev',
+        },
+    });
+    
+    
+    
+    
+    
+});
