c03c46ff03ac523f037d84da9b7ca0a38e8df8db
sujin <asj9674@gmail.comgit config --global user.email asj9674@gmail.com>
2020-12-02 02:24:30 +0900

    반응형 및 js적용
    반응형 _완료 (gnb 미완료)
    js _ gnb 부분, swiper , isotope 적용

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,36 @@
-$(function(){
-    
+$(function () {
+
+    var win_w = $(window).width();
+
+    $(window).on('resize',function(){
+        win_w = $(this).width();
+    });
+
+/*---------gnb menu------------------------------------------------------*/ 
+    $('#gnb').on('mouseenter',function(){
+        if(win_w >= 980){
+            $('.sub_menu').stop(true,true).slideDown();
+            $('.bgGnb').stop(true,true).slideDown();
+            $('.bgGnb').addClass('on');
+
+        } else{
+
+        }
+    });
+
+    $('.bgGnb').on('mouseleave',function(){
+        if(win_w >= 980){
+            $('.sub_menu').stop(true,true).slideUp();
+            $('.bgGnb').stop(true,true).slideUp();
+        }
+    });
+
+
+
+
+
+
+/*---------flavor------------------------------------------------------*/      
     var mySwiper = new Swiper('#flavor .left_box', {
         loop: true,
         navigation: {
@@ -7,18 +38,41 @@ $(function(){
             prevEl: '.swiper-button-prev',
         },
     });
-    
+
     var mySwiper = new Swiper('#flavor .right_box', {
         loop: true,
-        effect : 'fade',
+        effect: 'fade',
         navigation: {
             nextEl: '#flavor .swiper-button-next',
             prevEl: '#flavor .swiper-button-prev',
         },
     });
-    
-    
-    
-    
-    
+
+
+/*---------icecream------------------------------------------------------*/  
+    //isotope 
+    $('.grid').isotope({
+        itemSelector: '.icecream_item',
+    });
+
+    // filter items on button click
+    $('.btn_list > li').on('click', function (e) {
+
+        e.preventDefault();
+
+        var filterValue = $(this).children().attr('data-filter');
+
+        $('.grid').isotope({
+            filter: filterValue
+        });
+        $(this).addClass('on').siblings().removeClass('on');
+
+    });
+
+
+
+
+
+
+
 });
